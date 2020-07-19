08cb7c10485f8b56172f9c5b296962d773345236
VictorKrchv <bublikoo228@gmail.com>
2020-07-19 19:33:48 +0300

    finish base excel logic

---
+++ src/core/Emitter.js
@@ -0,0 +1,30 @@
+export class Emitter {
+    constructor() {
+        this.listeners = {}
+    }
+
+    // dispatch, fire, trigger
+    // Уведомляем слушателя если он есть
+    emit(event, ...args) {
+        if (!Array.isArray(this.listeners[event])) {
+            return false
+        }
+        this.listeners[event].forEach(listener => {
+            listener(...args)
+        })
+        return true
+    }
+
+    // on, listen
+    // подписываемся на уведомление
+    // Добавляем нового слушателя
+    subscribe(event, fn) {
+        this.listeners[event] = this.listeners[event] || []
+        this.listeners[event].push(fn)
+        return () => {
+            this.listeners[event] =
+                this.listeners[event].filter(listener => listener !== fn)
+        }
+    }
+}
+
